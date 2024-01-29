ff4661bf50753569e1df25b776b91425b836f0eb
kiboma <omayioben@gmail.com>
2024-01-29 08:55:18 +0300

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
     <header>
       <Link to="/" className="logo">
         <img src={Logo} alt="Shopmate Logo" />
-        <span>Shopping Cart</span>
+        <span>Redux Cart</span>
       </Link>
       <nav className="navigation">
         <NavLink to="/" className="link" end>Home</NavLink>
