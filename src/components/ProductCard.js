2938536330d5ccb5d69aae0700766fa76143f989
kiboma <omayioben@gmail.com>
2024-01-30 09:00:53 +0300

    Style home

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,20 +1,41 @@
-import { useDispatch } from "react-redux";
-import { add } from "../store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { add,remove } from "../store/cartSlice";
 
 import "./ProductCard.css";
 
+
 export const ProductCard = ({product}) => {
-  const {name, price, image} = product;
 
   const dispatch = useDispatch();
+  const [inCart, setInCart]=useState(false);
+  const {id, name, price, image} = product;
+  const cartList = useSelector(state => state.cartState.cartList);
 
+  const isInCart = cartList.find(item => item.id === id)
+
+  useEffect(() =>{
+    if (isInCart) {
+      setInCart(true);
+    } else {
+      setInCart(false);
+    }
+  },[cartList,id])
+
+
+ 
   return (
     <div className="productCard">
       <img src={image} alt={name} />
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button onClick={()=>dispatch(add(product))} >Add To Cart</button>
+        { inCart? (
+          <button onClick={()=>dispatch(remove(product))} style={{backgroundColor: 'red' }} >Remove</button>
+        ):(
+          <button onClick={()=>dispatch(add(product))} >Add To Cart</button>
+        )}
+        
       </div>
     </div>
   )
