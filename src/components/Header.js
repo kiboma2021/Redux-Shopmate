23474da2e35e41ebd4c8a83fd5c4e97e6413d868
kiboma <omayioben@gmail.com>
2024-01-30 08:36:17 +0300

    Count products in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-import { Link, NavLink } from "react-router-dom"; 
+import { Link, NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Logo from "../assets/logo.png"
 import "./Header.css";
 
 export const Header = () => {
+  const products_count = useSelector(state=> state.cartState.cartList).length
   return (
     <header>
       <Link to="/" className="logo">
@@ -14,7 +16,7 @@ export const Header = () => {
         <NavLink to="/cart" className="link">Cart</NavLink>
       </nav>
       <Link to="/cart" className="items">
-        <span>Cart: 2</span>
+        <span>Cart: {products_count}</span>
       </Link>
     </header>
   )
